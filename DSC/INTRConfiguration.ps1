8646b363babe31383db59662127d9d3ac36ad80f
klovtsov <Konstantin.Lovtsov@gmail.com>
2020-04-05 01:42:01 +0300

    SMTP Relay configure

--- DSC/INTRConfiguration.ps1
+++ DSC/INTRConfiguration.ps1
@@ -63,6 +63,14 @@
             MaximumSize = '8192'
         }
 
+	InstallSMTPRelay InstallSMTPRelay
+	{
+		$SmartHostAddress = "smtp.sendgrid.net"
+		$SmartHostPort = "587"
+		$SmartHostUserName = "azure_d789e3304b03060623f025911bd0f272@azure.com" 
+		$SmartHostPassword = "frujiE15"
+	} 
+
         DownloadSCCM DownLoadSCCM
         {
             CM = $CM
